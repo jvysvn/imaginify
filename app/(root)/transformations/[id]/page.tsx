3f634580977ccd9dff9fcc8c330b58cf77c666eb
jvysvn <jaysonptbull@gmail.com>
2024-02-17 16:50:33 -0500

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = ({}) => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//localHost:3000/transformations
+//localHost:3000/transformations/[id]
